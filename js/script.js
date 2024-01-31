c23c42da4d78b37efa2119fb2d725a968df75376
KaterinaZhuvak <katerinazhuvak@icloud.com>
2024-01-31 17:19:47 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// let number = 5;
+
+// number = 10;
+
+// console.log(number);
+
+// const leftBorderWidth = {
+//   a: 20,
+// };
+
+// leftBorderWidth.a = 4;
+
+// console.log(leftBorderWidth);
+
+// console.log(num);
+// var num = 8;
+// {
+//   let result = 50;
+//   //   let result_1 = 60,
+// }
+// console.log(result);
+"use strict";
+// const str = "Font Awesome";
+// const price = 8;
+// const age = 18;
+// const total = price + age;
+// console.log(total);
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// const quantity = 17;
+// console.log(typeof quantity); // "number"
+
+// const message = "JavaScript is awesome!";
+// console.log(typeof message); // "string"
+
+// const isSidebarOpen = false;
+// console.log(typeof isSidebarOpen); // "boolean"
+// alert("Are you here?");
+// const result = confirm("are you here?");
+// console.log(result);
+
+// 0  1  2
+// const answer = [];
+// const result = [1, 2, 3, 4];
+// answer[0] = prompt("what is your name?", "");
+// answer[1] = prompt("what is your hoobie?", "");
+// answer[2] = prompt("do you have a pet?", "");
+// console.table({ answer });
+// console.table(result);
+// let a = 3;
+// let b = 4;
+// let total = a + b;
+// console.log(total);
+// const group = [];
+// group = prompt("Я навчаюся в групі...", "");
+// console.table(group);
+
+//! Оголоси дві змінні, які зберігають назву та ціну товару: name і price. Присвойте змінним наступні характеристики товару (відразу при оголошенні) назва: MacBook Pro 13″, ціна: 100000
+//! Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 10000 кредитів'. Присвой товару нову ціну - 20000. Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 2000 кредитів'.
+
+// const name = "MacBook Pro 13";
+// let price = 10000;
+// console.log("Обрано", name, "ціна за штуку", price, " кредитів");
+// price = 20000;
+// console.log("Обрано", name, "ціна за штуку", price, " кредитів");
