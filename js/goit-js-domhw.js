e9e940431ecf412f18f860dfdd844cc38558782f
KaterinaZhuvak <129537035+KaterinaZhuvak@users.noreply.github.com>
2024-06-17 13:50:14 +0100

    new commit

--- js/goit-js-domhw.js
+++ js/goit-js-domhw.js
@@ -25,11 +25,6 @@ categoryName.forEach(item =>{
 //Task 2
 // Напиши скрипт, який для кожного елемента масиву ingredients створить окремий li, після чого вставить всі li за одну операцію в 
 //список ul.ingredients. Для створення DOM-вузлів використовуй document.createElement().
-
-const ingridientsList = document.getElementById("ingridients")
-ingridientsList.forEach(ingridients =>{
-    
-})
 const ingredients = [
     'Картопля',
     'Гриби',
@@ -38,3 +33,67 @@ const ingredients = [
     'Зелень',
     'Приправи',
    ];
+   const ingredientsList = document.getElementById('ingredients');
+   const fragment = document.createDocumentFragment();
+ 
+   ingredients.forEach(ingredient => {
+     const li = document.createElement('li');
+     li.textContent = ingredient;
+     fragment.appendChild(li);
+   });
+ 
+   ingredientsList.appendChild(fragment);
+
+//task 3
+   const images = [
+    {
+     url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     alt: 'White and Black Long Fur Cat',
+    },
+    {
+     url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+     url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     alt: 'Group of Horses Running',
+    },
+   ];
+
+   const gallery = document.getElementById('gallery');
+   const galleryItems = images.map(image => `
+     <li>
+       <img src="${image.url}" alt="${image.alt}">
+     </li>
+   `);
+ 
+   gallery.insertAdjacentHTML('beforeend', galleryItems);
+
+   //task 4
+   let counterValue = 0;
+
+   const valueSpan = document.getElementById('value');
+   const decrementButton = document.querySelector('[data-action="decrement"]');
+   const incrementButton = document.querySelector('[data-action="increment"]');
+ 
+   const updateValue = () => {
+     valueSpan.textContent = counterValue;
+   };
+ 
+   const decrement = () => {
+     counterValue -= 1;
+     updateValue();
+   };
+ 
+   const increment = () => {
+     counterValue += 1;
+     updateValue();
+   };
+ 
+   decrementButton.addEventListener('click', decrement);
+   incrementButton.addEventListener('click', increment);
+ 
+   updateValue();
