e9f03d6a4679bcfd5280e99a64bf6594343b94e2
KaterinaZhuvak <katerinazhuvak@icloud.com>
2024-09-17 22:00:25 +0100

    new commit

---
+++ lib_frame/common-js/greeter.js
@@ -0,0 +1,18 @@
+// const helloMessage = 'Hello!'
+// const goodbyeMessage = 'Goodbye!'
+
+// const hello =()=> helloMessage
+// const goodbye =()=> goodbyeMessage
+
+// module.exports = {hello, goodbye}
+
+// const filterModule {
+
+// }
+const helloMessage = "Hello!";
+const goodbyeMessage = "Goodbye!";
+
+const hello = () => helloMessage
+const goodbye = () => goodbyeMessage
+
+module.exports = { hello, goodbye, helloMessage, goodbyeMessage }
---
+++ lib_frame/common-js/index.js
@@ -0,0 +1,10 @@
+// const greeter = require('./greeter')
+// console.log(greeter.hello());
+// console.log(greeter.goodbye());
+
+const greeter = require('./greeter')
+
+console.log(greeter.hello());
+console.log(greeter.goodbye());
+console.log(greeter.helloMessage);
+console.log(greeter.goodbyeMessage);
